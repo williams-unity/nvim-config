9a4b40def276d4b0721cd6b4e7b85700b824ac29
William Sprent <williams@unity3d.com>
2024-12-09 15:02:06 +0100

    init: elixir_ls

--- init.lua
+++ init.lua
@@ -638,6 +638,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = gopls_opts,
+        elixirls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
