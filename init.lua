ba350ea7b8ca26163c9b1226467019840f239dce
William Sprent <williams@unity3d.com>
2024-12-09 15:01:10 +0100

    init: +mini.jump2, jump to word config

--- init.lua
+++ init.lua
@@ -893,6 +893,36 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+      require('mini.jump2d').setup()
+
+      vim.keymap.set({ 'n', 'v' }, '<leader>jw', function()
+        print 'Press key: '
+        local c = vim.fn.getcharstr()
+        print ''
+        local lower = c:lower()
+        local upper = c:upper()
+        local cclass = '[' .. lower .. upper .. ']'
+
+        local startline = MiniJump2d.gen_pattern_spotter('^' .. cclass, 'start')
+        local inline = MiniJump2d.gen_pattern_spotter('[%s%p]()' .. cclass .. '[^%s%p]*', 'none')
+        MiniJump2d.start {
+          spotter = function(linenum, args)
+            local a = startline(linenum, args)
+            local b = inline(linenum, args)
+            if a == nil then
+              return b
+            end
+            if b == nil then
+              return a
+            end
+            for _, v in pairs(a) do
+              table.insert(b, v)
+            end
+            return b
+          end,
+          allowed_lines = { blank = false },
+        }
+      end, {})
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
