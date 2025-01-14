0ce2a3531dedca67416da839dd1faa385a96bac3
William Sprent <williams@unity3d.com>
2025-01-14 16:22:46 +0100

    plugins/oil: show hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,11 @@ return {
   opts = {},
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
     vim.keymap.set('n', '<leader>-', '<cmd>Oil<cr>', { desc = 'Open parent dir' })
   end,
 }
