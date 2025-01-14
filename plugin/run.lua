a4e8b5cd9c30c82737f686df722642e58edb74a9
William Sprent <williams@unity3d.com>
2025-01-14 16:23:11 +0100

    plugin/run.lua: iteration

--- plugin/run.lua
+++ plugin/run.lua
@@ -2,9 +2,10 @@ local state = {
   win_nr = -1,
   buf_nr = -1,
   last_command = nil,
+  last_command_opts = nil,
 }
 
-local attach_terminal = function(buf_nr)
+local attach_terminal_below = function(buf_nr)
   local cur_pos = vim.api.nvim_win_get_position(0)
   local lowest_win = 0
   local lowest_col = 0
@@ -25,9 +26,30 @@ local attach_terminal = function(buf_nr)
   -- end
 end
 
+local attach_terminal_right = function(buf_nr)
+  local cur_pos = vim.api.nvim_win_get_position(0)
+  local rightest_win = 0
+  local rightest_col = 0
+  for _, win in pairs(vim.api.nvim_list_wins()) do
+    local pos = vim.api.nvim_win_get_position(win)
+    if pos[2] > rightest_col then
+      rightest_win = win
+      rightest_col = pos[2]
+    end
+  end
+  state.win_nr = vim.api.nvim_open_win(buf_nr, true, {
+    split = 'right',
+    height = math.floor(vim.o.columns * 0.4),
+    win = rightest_win,
+  })
+end
+
+local attach_terminal = attach_terminal_right
+
 local create_split = function()
   if not vim.api.nvim_buf_is_valid(state.buf_nr) then
-    state.buf_nr = vim.api.nvim_create_buf(false, true)
+    state.buf_nr = vim.api.nvim_create_buf(true, false)
+    vim.api.nvim_buf_set_name(state.buf_nr, '*Command*')
   end
 
   if not vim.api.nvim_win_is_valid(state.win_nr) then
@@ -64,18 +86,50 @@ end
 
 local term_command = function(cur_win, win, _, cmd, args)
   args = args or {}
-  if args.cwd then
-    table.insert(cmd, 1, '&&')
-    table.insert(cmd, 1, args.cwd)
-    table.insert(cmd, 1, 'cd')
-  end
-  vim.api.nvim_set_current_win(win)
-  vim.api.nvim_cmd({ cmd = 'terminal', args = cmd }, {})
+  -- if args.cwd then
+  --   table.insert(cmd, 1, '&&')
+  --   table.insert(cmd, 1, args.cwd)
+  --   table.insert(cmd, 1, 'cd')
+  -- end
+  -- vim.api.nvim_set_current_win(win)
+  -- vim.api.nvim_cmd({ cmd = 'terminal', args = cmd }, {})
+
   local buf = vim.api.nvim_win_get_buf(win)
+  vim.api.nvim_set_current_win(cur_win)
+  -- vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
+
+  local chan = vim.api.nvim_open_term(buf, {})
   local line_count = vim.api.nvim_buf_line_count(buf)
   vim.api.nvim_win_set_cursor(win, { line_count, 0 })
-  vim.api.nvim_set_current_win(cur_win)
+  local write_to_term = function(text)
+    -- vim.api.nvim_buf_set_lines(buf, -2, -1, false, text)
+    for _, data in ipairs(text) do
+      vim.api.nvim_chan_send(chan, data)
+    end
+  end
+  print(args.cwd)
+  write_to_term { 'workdir=', args.cwd or vim.uv.cwd(), '; ', 'cmd=', table.concat(cmd, ' '), '\r\n\r\n' }
+
+  local handle_output = function(_, data)
+    if not data or #data == 0 then
+      return
+    end
+    write_to_term { data }
+  end
+  handle_output = vim.schedule_wrap(handle_output)
+  vim.system(cmd, {
+    cwd = args.cwd,
+    stdout = handle_output,
+    stderr = handle_output,
+    text = true,
+  }, function(sc)
+    vim.schedule(function()
+      write_to_term { '\n', '[Process exited ', tostring(sc.code), ']' }
+    end)
+  end)
+
   state.last_command = cmd
+  state.last_command_opts = args
 end
 
 local run_last_command = function()
@@ -91,7 +145,7 @@ local run_last_command = function()
   end
 
   -- send_keys_and_back(cur_win, state.win_nr, state.buf_nr, '\x1b[A\n')
-  term_command(cur_win, state.win_nr, state.buf_nr, state.last_command, {})
+  term_command(cur_win, state.win_nr, state.buf_nr, state.last_command, state.last_command_opts)
 end
 
 vim.api.nvim_create_user_command('TT', function(args)
@@ -100,18 +154,23 @@ vim.api.nvim_create_user_command('TT', function(args)
   -- send_keys_and_back(cur_win, state.win_nr, state.buf_nr, args.args .. '\n')
   term_command(cur_win, state.win_nr, state.buf_nr, args.fargs, {})
 end, {
-  nargs = '?',
+  nargs = '*',
   complete = 'shellcmd',
 })
 
+local trim_wd = function(wd)
+  wd = string.gsub(wd, '^oil://', '')
+  return wd
+end
+
 vim.api.nvim_create_user_command('TD', function(args)
-  local cwd = vim.fn.expand '%:h'
+  local cwd = trim_wd(vim.fn.expand '%:h')
   local cur_win = vim.api.nvim_get_current_win()
   create_split()
   -- send_keys_and_back(cur_win, state.win_nr, state.buf_nr, args.args .. '\n', { cwd = cwd })
   term_command(cur_win, state.win_nr, state.buf_nr, args.fargs, { cwd = cwd })
 end, {
-  nargs = '?',
+  nargs = '*',
   complete = 'shellcmd',
 })
 
